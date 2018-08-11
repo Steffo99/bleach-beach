0ccf376e126850af734d352cd4ddbb41bef983c8
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-08-11 13:02:45 +0200

    TUTTO

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public Rigidbody2D rigidbody;
+    public Rigidbody2D rb;
     public float horizontalShipSpeed = 35;
     public float verticalShipSpeed = 10;
 
     void Start () {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	void FixedUpdate () {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.localScale = new Vector3(-1, 1, 1);
-            rigidbody.AddForce(Vector2.left * horizontalShipSpeed);
+            rb.AddForce(Vector2.left * horizontalShipSpeed);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.localScale = new Vector3(1, 1, 1);
-            rigidbody.AddForce(Vector2.right * horizontalShipSpeed);
+            rb.AddForce(Vector2.right * horizontalShipSpeed);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rigidbody.AddForce(Vector2.up * verticalShipSpeed);
+            rb.AddForce(Vector2.up * verticalShipSpeed);
         }
     }
 }
