7ab2a3e247ca5980605cc9ddbd2772cb085d6f61
Lorenzo Balugani <lorenzo.balugani@gmail.com>
2018-08-11 11:44:50 +0200

    Add Venice in Project
    Added the Tide control script

---
+++ Assets/Scripts/Marea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marea : MonoBehaviour {
+    public BuoyancyEffector2D buoyancyEffector2D;
+    float speed = 0.01f;
+	// Use this for initialization
+	void Start () {
+        buoyancyEffector2D = GetComponent<BuoyancyEffector2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(buoyancyEffector2D.surfaceLevel>0)
+        {
+            buoyancyEffector2D.surfaceLevel = buoyancyEffector2D.surfaceLevel - (Time.deltaTime * speed);
+        }
+	}
+}
