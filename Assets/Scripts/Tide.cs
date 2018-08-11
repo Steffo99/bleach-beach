1c5886f5dee44850451688be25430d1acd898740
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-08-11 11:53:33 +0200

    .

--- Assets/Scripts/Tide.cs
+++ Assets/Scripts/Tide.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Marea : MonoBehaviour {
-    public BuoyancyEffector2D buoyancyEffector2D;
-    float speed = 0.01f;
-	// Use this for initialization
+public class Tide : MonoBehaviour {
+
+    private BuoyancyEffector2D buoyancyEffector2D;
+    public float speed = 0.01f;
+
 	void Start () {
         buoyancyEffector2D = GetComponent<BuoyancyEffector2D>();
     }
 	
-	// Update is called once per frame
 	void Update () {
-        if(buoyancyEffector2D.surfaceLevel>0)
+        if(buoyancyEffector2D.surfaceLevel > 0)
         {
             buoyancyEffector2D.surfaceLevel = buoyancyEffector2D.surfaceLevel - (Time.deltaTime * speed);
         }
