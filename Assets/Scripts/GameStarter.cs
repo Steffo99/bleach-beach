df4761c7062d883c35cb2d10499d01cfe61a3d41
Lorenzo Balugani <lorenzo.balugani@gmail.com>
2018-08-12 16:22:39 +0200

    Add fade animation
    It does not work, btw

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameStarter : MonoBehaviour {
+    public Image image;
+    public Animator animator;
 
     void OnTriggerEnter2D(Collider2D GameStart)
     {
+        animator.SetBool("Fade", false);
         Application.LoadLevel(1);
-        Debug.Log("Hi Mark");
-
     }
-
 }
