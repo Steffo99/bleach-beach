6f8616a9d02f6aa85c21bc3c87d1d438a5bb7f2c
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-08-12 14:36:01 +0200

    ???

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -10,16 +10,13 @@ public class Cannon : MonoBehaviour {
     public float forceBuildingSpeed = 2f;
     public float startingForce = 1f;
     private float force = 1f;
-
-    public GameObject chain;
-
-    private Linker chainLinker;
+    
     private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        chainLinker = chain.GetComponent<Linker>();        
+        Physics2D.IgnoreLayerCollision(8, 8);
     }
 
     private void Update ()
@@ -41,10 +38,14 @@ public class Cannon : MonoBehaviour {
                 harpoonInstance.transform.rotation = transform.rotation;
                 harpoonInstance.GetComponent<Rigidbody2D>().AddForce(direction * force);
                 harpoonInstance.GetComponent<SpriteRenderer>().flipX = spriteRenderer.flipX;
-                chainLinker.secondObject = harpoonInstance;
+                Linker linker = harpoonInstance.GetComponentInChildren<Linker>();
+                linker.firstObject = gameObject;
+                linker.secondObject = harpoonInstance;
                 force = startingForce;
                 harpoonAvailable = false;
             }
         }
+        float scale = 1f + (((force - startingForce) / (maxForce - startingForce)) * 0.5f);
+        transform.localScale = new Vector3(scale, scale, scale);
     }
 }
--- Assets/Scripts/Harpoon.cs
+++ Assets/Scripts/Harpoon.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class Harpoon : MonoBehaviour {
 
+    public float returnSpeed = 5f;
+
     private Rigidbody2D rigidbody2D;
     private bool going = true;
+    private Linker linker;
 
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        linker = GetComponentInChildren<Linker>();
     }
 
-    void Update () {
+    void Update ()
+    {
         if(going)
         {
             transform.rotation = Quaternion.LookRotation(Vector3.forward, rigidbody2D.velocity.normalized);
         }
         else
         {
+            if (transform.position.y > linker.firstObject.transform.position.y)
+            {
 
+                rigidbody2D.MoveRotation(Vector2.Angle(transform.position - linker.firstObject.transform.position, Vector2.right) - 90f);
+            }
+            else
+            {
+
+                rigidbody2D.MoveRotation(-Vector2.Angle(transform.position - linker.firstObject.transform.position, Vector2.right) - 90f);
+            }
+            rigidbody2D.MovePosition(Vector3.MoveTowards(transform.position, linker.firstObject.transform.position, returnSpeed * Time.deltaTime));
+            if(Vector3.Distance(transform.position, linker.firstObject.transform.position) < 0.2f)
+            {
+                linker.firstObject.GetComponent<Cannon>().harpoonAvailable = true;
+                Destroy(gameObject);
+            }
         }
 	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        going = false;
+        rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+    }
 }
